d1047d4fcac485305220a2afef44faa4e26b5936
hpuliyedath <hpuliyedath@vlocity.com>
2019-06-30 19:11:07 -0700

    remove unused dependencies

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 require('./styles/main.scss');
 const React = require('react');
-const { createLogger } = require('redux-logger');
 const { render } = require('react-dom');
-const { Provider } = require('react-redux');
-const { createStore, applyMiddleware } = require('redux');
-const { rootReducer } = require('./reducers');
 
 const MainComponent = require('./components/MainComponent');
 
-const logger = createLogger();
-
-const store = createStore(
-  rootReducer,
-  applyMiddleware(
-    logger,
-  ));
-
 render(
-    <Provider store={store}>
-      <MainComponent />
-    </Provider>,
-  document.getElementById('root')
+    <MainComponent />,
+    document.getElementById('root')
 );
